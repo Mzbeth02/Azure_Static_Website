3e4e626baf1afefc1e4a18f48fa3e10e30d23fe5
Adedamola <adedamola.ayeni@avonhealthcare.com>
2025-10-07 22:23:53 +0100

    git bash practice

--- Scripts/script2.sh
+++ Scripts/script2.sh
@@ -2,4 +2,10 @@
 # This script is to understand bash
 # name is a variable
 test='Techcrush'
-echo "Welcome $test Cloud Computing Scholars, $test is the best, you would learn from $test"
+test='Oleey Academy'
+readonly color="blue"
+echo "Welcome to $test Cloud Computing Scholars, $test is the best, you would learn from $test"
+echo "my favourite color is $color"
+
+a=$(ls)
+echo "$a"
